1d81cdfd6b917fe14c36e00f166ff177c282a1ad
Indiance <tharun.tvishy@gmail.com>
2022-02-27 22:55:32 +0530

    adding new stuff and returning

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,3 @@
--- global options
 vim.g.mapleader = ','
 vim.o.number = true
 vim.o.backspace = [[indent,eol,start]]
@@ -10,49 +9,43 @@ vim.o.updatetime = 50
 vim.o.shortmess = vim.o.shortmess .. "c"
 vim.o.mouse = "a"
 vim.o.background = "dark"
--- window options
 vim.wo.relativenumber = true
 vim.wo.wrap = false
 vim.wo.spell = true
 vim.wo.scrolloff = 8
 vim.wo.colorcolumn = "80"
 vim.wo.spell = false
--- buffer options
 vim.bo.expandtab = true
 vim.bo.shiftwidth = 4
 vim.bo.tabstop = 4
 vim.bo.softtabstop = 4
 vim.bo.autoindent = true
 vim.bo.smartindent = true
--- package manager stuff
 require "paq" {
     "savq/paq-nvim";
-    -- lsp and autocompletion
     "neovim/nvim-lspconfig";
     "hrsh7th/nvim-compe";
     "tpope/vim-fugitive"; -- git manager
-    -- telescope and its dependencies
-    "morhetz/gruvbox";
     "nvim-telescope/telescope.nvim";
     "nvim-lua/plenary.nvim";
-    "nvim-lualine/lualine.nvim"; -- vim statusline
-    "kyazdani42/nvim-web-devicons";
+    "gruvbox-community/gruvbox";
+    "itchyny/lightline.vim";
+    "HendrikPetertje/vimify";
 }
--- vim commands
-vim.cmd('set guicursor=')
 vim.cmd('colorscheme gruvbox')
-vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
--- importing configuration files
 require('teleconfig')
 require('lsp')
 require('compeconfig')
--- making some keybindings kekw. This one enables buffer navigation
-vim.api.nvim_set_keymap('n', '<F8>', ':tabn<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('n', '<F7>', ':tabp<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F8>', ':SpPause<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F7>', ':SpPrevious<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F9>', ':SpNext<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<F10>', ':SpPlay<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_var('netrw_banner', 0)
 vim.api.nvim_set_var('netrw_liststyle', 3)
 vim.api.nvim_set_var('netrw_browsesplit', 4)
 vim.api.nvim_set_var('netrw_altv', 1)
 vim.api.nvim_set_var('netrw_winsize', 25)
-require('lualine').setup()
-require'nvim-web-devicons'.setup()
+vim.g['lightline'] = {
+  colorscheme = 'gruvbox',
+}
+vim.g['spotify_token']='ZTU3NDU1NGI1YTc2NGQ1NTg4MmIyZWFkNGI1NTliYjk6IDkwYzM3MTU0NGRkNzQ4MjM5Yzc3NmY4MjJmYWI4NmE3'
